ff76d0f78aedfc118b9798c5ebb30637c34c4081
gdp20260628 <gustavo.diaz.finance@gmail.com>
2026-06-28 20:20:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Grafica de serie de tiempo")
+st.title("Evolución de ventas")
 
 entrada = st.text_input("Ingrese la serie, separada por comas:",value="10,15,18,26,31")
 entrada2 = entrada.split(",")
